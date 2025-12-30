c4236cf6c844dc76df2027110010a8c8b2f1d572
Jan Piñol Castuera <jan.pinol@students.salle.url.edu>
2025-12-30 16:55:03 +0100

    Feature: Added controls menu to title screen with toggle navigation

---
+++ scripts/title_screen.gd
@@ -0,0 +1,33 @@
+extends Control
+
+@export var gameplay_scene: PackedScene
+
+@onready var main_menu: VBoxContainer = $CenterContainer/MainMenu
+@onready var controls_menu: VBoxContainer = $CenterContainer/ControlsMenu
+
+@onready var play_button: Button = $CenterContainer/MainMenu/PlayButton
+@onready var controls_button: Button = $CenterContainer/MainMenu/ControlsButton
+@onready var exit_button: Button = $CenterContainer/MainMenu/ExitButton
+@onready var back_button: Button = $CenterContainer/ControlsMenu/BackButton
+
+func _ready() -> void:
+	play_button.pressed.connect(_on_play_pressed)
+	controls_button.pressed.connect(_on_controls_pressed)
+	exit_button.pressed.connect(_on_exit_pressed)
+	back_button.pressed.connect(_on_back_pressed)
+
+func _on_play_pressed() -> void:
+	if gameplay_scene == null:
+		gameplay_scene = load("res://scenes/Main.tscn") as PackedScene
+	get_tree().change_scene_to_packed(gameplay_scene)
+
+func _on_controls_pressed() -> void:
+	main_menu.visible = false
+	controls_menu.visible = true
+
+func _on_back_pressed() -> void:
+	controls_menu.visible = false
+	main_menu.visible = true
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
